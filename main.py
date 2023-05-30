5912bb2637a58e44e635948a05673a926cd3ba65
Jakub <117046128+KLesiu@users.noreply.github.com>
2023-05-30 22:41:00 +0200

    Add bird movement

--- main.py
+++ main.py
@@ -11,8 +11,13 @@ background = pygame.image.load('img_1.png')
 
 # Create bird
 birdImg = pygame.image.load('flappy.png')
-birdX = 150
+
 birdY = 180
+birdY_change = 0.2
+
+
+def bird(x,y):
+    screen.blit(birdImg, (x, y))
 
 
 # Game loop
@@ -21,8 +26,20 @@ running = True
 while running:
     screen.fill((0,0,0))
     screen.blit(background, (0, 0))
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    screen.blit(birdImg, (birdX,birdY))
+
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                birdY_change = -0.4
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_SPACE:
+                birdY_change = 0.2
+    # Bird movement
+    birdY += birdY_change
+    bird(150,birdY)
+
     pygame.display.update()
